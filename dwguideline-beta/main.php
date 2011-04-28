82be7017abfc6d61c830cb4e88144c486e768826
Peter Krantz <peter.krantz@gmail.com>
2011-04-28 14:18:32 +0200

    Analytics-skript

--- dwguideline-beta/main.php
+++ dwguideline-beta/main.php
@@ -102,5 +102,15 @@ if (!defined('DOKU_INC')) die();
 </div>
 <div class="no"><?php /* provide DokuWiki housekeeping, required in all templates */ tpl_indexerWebBug()?></div>
 <?php /*old includehook*/ @include(dirname(__FILE__).'/footer.html')?>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '<?php echo $conf['ga_code']; ?>']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 </body>
 </html>
